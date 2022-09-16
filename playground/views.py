f934a889e05e0f459d98bbcd3559886b58591ece
Sanja Strbac <strbac.m.sanja@gmail.com>
2022-09-16 17:54:56 +0200

    lesson50

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from django.db.models import Q, F
+from store.models import Product, OrderItem
 
 # Create your views here.
 
 def say_hello(request):
+    queryset = Product.objects.only('id', 'title')
 
-    queryset = Product.objects.filter(title__icontains = 'coffee')
 
 
     return render(request, 'hello.html', {'name': 'Sanja', 'products': list(queryset)})
